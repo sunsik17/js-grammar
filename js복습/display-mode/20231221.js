df77e0b0f551fb388796564098479efd97fc300c
sunsik17 <sunsig21c@gmail.com>
2023-12-21 15:08:19 +0900

    js 복습 - 디스플레이 모드 바꾸기 버튼 활성화

---
+++ js복습/display-mode/20231221.js
@@ -0,0 +1,7 @@
+function changeBgColor(id) {
+    if (id === 'dark') {
+        document.body.style.backgroundColor = 'black'  
+    } else {
+        document.body.style.backgroundColor = 'yellow'  
+    }
+}
